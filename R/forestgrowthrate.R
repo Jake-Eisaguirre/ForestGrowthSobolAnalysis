f1053df478358ad74e8f4599689927005b1c438b
tinafang-1207 <yutianfang@ucsb.edu>
2022-05-13 16:44:09 -0700

    Finish part 1 and part 2 of the assignment

---
+++ R/forestgrowthrate.R
@@ -0,0 +1,22 @@
+#'  Forest growth rate
+#' @param time period of growth (years)
+#' @param C size of the forest (kg C)
+#' @param parms - as list with four values, r, g, K and threshold
+#' @param parms$r - exponential forest growth rate
+#' @param parms$g - linear forest growth rate (kg/year)
+#' @parms parms$K - carrying capacity (kg C)
+#' @parms parms$threshold - canopy closure threshold (kg C)
+#' @return change in forest size with time
+#'
+dgrowthrate = function(time, C, parms) {
+  
+  # compute rate of forest growth when C < threshold
+  dC = parms$r*C
+  
+  # compute rate of forest growth when C > = threshold
+  if(C >= parms$threshold) {
+    dC = parms$g * (1 - C/parms$K)
+    
+  }
+  return(list(dC))
+}
